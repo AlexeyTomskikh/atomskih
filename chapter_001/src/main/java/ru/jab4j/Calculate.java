1f5c09e98455799c140f3902f735f64e768819a0
AlexeyTomskikh <alexey.tomskih@gmail.com>
2016-11-27 23:30:12 +0700

    Comment text

--- chapter_001/src/main/java/ru/jab4j/Calculate.java
+++ chapter_001/src/main/java/ru/jab4j/Calculate.java
@@ -1,9 +1,9 @@
-package ru.atomskih;
+package ru.jab4j;
 
 public class Calculate{
 	public static void main(String[] args){
 
 		System.out.println("Hello, world!");
-                            }
+                   }
 
 }
