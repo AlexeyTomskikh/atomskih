ee6200eaa1c5b9bf2ef720c4498e4377b52ef3ba
AlexeyTomskikh <alexey.tomskih@gmail.com>
2016-12-01 23:01:41 +0700

    Original Commit2

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,18 @@
 package ru.job4j;
 
 /**
-* Class Calculate решение задачи части 001 урок1.
-* @author Tomskih
-* @since 05/11/2016
-*/
-
-public class Calculate{
-	public static void main(String[] args){
-
-		System.out.println("Hello, world!");
-                   }
-
+ * Calculate.
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
