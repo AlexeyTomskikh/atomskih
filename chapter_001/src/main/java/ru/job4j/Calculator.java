2b012872c5ebca2bbd86b4e5900f9c36f30cbb24
AlexeyTomskikh <alexey.tomskih@gmail.com>
2016-12-03 00:09:22 +0700

    Add Calculator and CalculatorTest

---
+++ chapter_001/src/main/java/ru/job4j/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+double result;
+
+
+	public void add(double first, double second) {
+	this.result = first + second;
+	}
+	
+	public double getResult() {
+	return result;
+	}
+
+
+}
