ed1cc3c1d5e002f8725cce558f24cdb54915b093
AlexeyTomskikh <alexey.tomskih@gmail.com>
2017-01-07 02:52:26 +0700

    Correction task 4.3,real final version

--- chapter_001/src/main/java/ru/job4j/Paint.java
+++ chapter_001/src/main/java/ru/job4j/Paint.java
@@ -4,15 +4,15 @@ public class Paint {
 
     public String piramid(int h) {
         StringBuilder line1 = new StringBuilder();
-        for (int i = 0; i < h; i++) {   // цикл считает строки
-            for (int j = i; j < h; j++) { //цикл печатает пробелы перед пирамидкой
+        for (int i = 0; i < h; i++) {
+            for (int j = i; j < h; j++) {
                 line1.append(" ");
             }
-            for(int k = 0; k <= i; k++) { // цикл печатает ^ в строках
+            for (int k = 0; k <= i; k++) {
                 line1.append("^");
-                if (k < i){
+                if (k < i) {
                     line1.append(" ");
-                }else {
+                } else {
                     line1.append("\n");
                 }
             }
