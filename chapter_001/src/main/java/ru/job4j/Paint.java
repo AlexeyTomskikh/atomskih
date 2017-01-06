dadd8fed59d7a7220430da3fdeb28d6da83efa65
AlexeyTomskikh <alexey.tomskih@gmail.com>
2017-01-07 02:02:24 +0700

    Correction test for task 4.3

--- chapter_001/src/main/java/ru/job4j/Paint.java
+++ chapter_001/src/main/java/ru/job4j/Paint.java
@@ -4,11 +4,18 @@ public class Paint {
 
     public String piramid(int h) {
         StringBuilder line1 = new StringBuilder();
-        for (int i = 0; i < h; i++) {
-            for (int j = i; j <= h; j++) {
-                System.out.print(" ");
+        for (int i = 0; i < h; i++) {   // цикл считает строки
+            for (int j = i; j < h; j++) { //цикл печатает пробелы перед пирамидкой
+                line1.append("1");
             }
-            line1.append("^ ");
+            for(int k = 0; k <= i; k++) { // цикл печатает ^ в строках
+                line1.append("^");
+                if (k < i){
+                    line1.append("2");
+                }else {
+                    line1.append("\n");
+                }
+            }
         }
         return line1.toString();
     }
--- chapter_001/src/test/java/ru/job4j/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/PaintTest.java
@@ -6,11 +6,10 @@ import static org.junit.Assert.assertThat;
 public class PaintTest {
 
     @org.junit.Test
-    public void piramidTest() throws Exception {
-
+    public void piramidTest() {
+        String expectedLine = "1111^\n111^2^\n11^2^2^\n1^2^2^2^\n";
         Paint paint = new Paint();
-        String line = paint.piramid(3);
-        int lengthString = line.length();
-        assertThat(lengthString, is(6));
+        //System.out.println(paint.piramid(3));
+        assertThat(paint.piramid(4), is(expectedLine));
     }
 }
